be2d6615e3c7ed256118d4a5b1a7d88f1204f534
jayathungek <jayathunge.work@gmail.com>
2023-11-07 16:00:12 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# L-systems 
+<p float="left">
+  <img src="img/banner.png" width= "30%" />
+  <img src="img/banner2.png" width="30%" />
+  <img src="img/banner3.png" width="30%" />
+</p>
+
+## Advanced python programming
+We will be learning how to code in python by implementing [Lindenmayer systems](https://en.wikipedia.org/wiki/L-system).
+
+According to Wikipedia:
+
+> An L-system or Lindenmayer system is a parallel rewriting system and a type of formal grammar. An L-system consists of an alphabet of symbols that can be used to make strings, a collection of production rules that expand each symbol into some larger string of symbols, an initial "axiom" string from which to begin construction, and a mechanism for translating the generated strings into geometric structures.
+
+We will cover:
+* Classes
+* Functions
+* Object oriented programming
+* Test driven development
+* Commandline arguments
+
+# Add to this document!
+Feel free to keep adding to this document, perhaps with your own observations or results from experiments :)
---
+++ pen.py
@@ -0,0 +1,79 @@
+from typing import Tuple
+import math
+
+from PIL import Image, ImageDraw
+
+
+class Pen:
+    def __init__(
+        self, image_dimensions: Tuple[int, int], pen_pos: Tuple = (0, 0), width: int = 0
+    ):
+        self.pos = pen_pos
+        self.heading = 0.0
+        self.is_down = False
+        self.bg = "#4C4C4C"
+        self.colour = "#EC99B2"
+        self.canvas_size = image_dimensions
+        self.image = Image.new("RGBA", self.canvas_size, self.bg)
+        self.drawing = ImageDraw.Draw(self.image)
+        self.width = width
+
+    def forward(self, dist: float):
+        newpos = (
+            (dist * math.cos(self.heading)) + self.pos[0],
+            (dist * math.sin(self.heading)) + self.pos[1],
+        )
+        if self.is_down:
+            self.drawing.line(self.pos + newpos, fill=self.colour, width=self.width)
+        self.pos = newpos
+
+    def left(self, heading: float):
+        radians = (heading * math.pi) / 180.0
+        self.heading -= radians
+
+    def right(self, heading: float):
+        radians = (heading * math.pi) / 180.0
+        self.heading += radians
+
+    def up(self):
+        self.is_down = False
+
+    def down(self):
+        self.is_down = True
+
+    def set_pos(self, newpos: Tuple):
+        self.pos = newpos
+
+    def set_width(self, new_width: int):
+        self.width = new_width
+
+    def set_heading(self, new_heading: float):
+        radians = (new_heading * math.pi) / 180.0
+        self.heading = radians
+
+    def get_pos(self) -> Tuple:
+        return self.pos
+
+    def get_heading(self) -> float:
+        degrees = (self.heading * 180) / math.pi
+        return degrees
+
+    def set_colour(self, new_colour: str):
+        self.colour = new_colour
+
+    def set_bg(self, new_bg: str):
+        self.bg = new_bg
+
+    def set_canvas_size(self, size: Tuple[int, int]):
+        self.canvas_size = size
+        self.image = Image.new("RGBA", self.canvas_size, self.bg)
+        self.drawing = ImageDraw.Draw(self.image)
+
+    def show(self):
+        self.image.show()
+
+    def save(self, filename):
+        self.image.save(filename, "PNG")
+
+    def get_image(self) -> Image.Image:
+        return self.image
