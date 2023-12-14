3a8eb1b5ca05ecb722f1fd99debfb453407583db
jayathungek <jayathunge.work@gmail.com>
2023-12-14 11:06:32 +0000

    added new tests for pen class

--- test_lsystem.py
+++ test_lsystem.py
@@ -1,6 +1,7 @@
 import unittest
 
-from lsystem import LSystem
+from pen import Pen
+from lsystem import LSystem, LTree
 
 
 class TestLSystem(unittest.TestCase):
@@ -20,6 +21,38 @@ class TestLSystem(unittest.TestCase):
 
         # Check if final answer is what we would expect
         self.assertEqual(state, "aba")
+
+
+class TestPen(unittest.TestCase):
+    def setUp(self):
+        self.pen = Pen(
+            image_dimensions=(1000, 1000),
+            pen_pos=(500, 1000),
+            width=3
+        )
+        self.pen.set_heading(-90)
+
+    def test_position(self):
+        new_positions = [
+            (x, y) 
+            for x in range(0, 1000, 100)
+            for y in range(0, 1000, 100)
+        ]
+
+        for pos in new_positions:
+            self.pen.set_pos(pos)
+            self.assertEqual(pos, self.pen.pos)
+    
+    def test_move_up(self):
+        distance = 50
+        self.pen.forward(distance)
+        self.assertEqual(self.pen.pos, (500, 950))
+        
+    def test_move_left(self):
+        distance = 50
+        self.pen.left(90)
+        self.pen.forward(distance)
+        self.assertEqual(self.pen.pos, (450, 1000))
 
 if __name__ == "__main__":
     unittest.main()
